d444c8eca92e6d01e54887344950fd5ff6576e53
slitt9 <sahillitt@hotmail.com>
2025-05-03 15:35:12 -0700

    fixed NavBar for Mobile Users

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,53 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logoImg from "../assets/sikhyapunjablogo.png"; // Import your logo image
-
+import logoImg from "../assets/sikhyapunjablogo.png";
 
 const Navbar = () => {
-  return (
-      <nav className="navbar">
-        <div className="logo">
-          <Link to="/">
-            <img src={logoImg} alt="Sikhya Punjab Logo" className="logo-image" />
-          </Link>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        </div>
-        <ul>
-          <li>
-            <Link to="/lessons-speaking">Test Your Speaking</Link>
-          </li>
-          <li>
-            <Link to="/lessons-reading">Test Your Reading</Link>
-          </li>
-          <li>
-            <Link to="/important-figures">Important Figures</Link>
-          </li>
-          <li>
-            <Link to="/important-events">Important Events</Link>
-          </li>
-          <li>
-            <Link to="/values-ethics">Values & Ethics</Link>
-          </li>
-        </ul>
-      </nav>
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">
+          <img src={logoImg} alt="Sikhya Punjab Logo" className="logo-image" />
+        </Link>
+      </div>
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <li>
+          <Link to="/lessons-speaking" onClick={() => setMenuOpen(false)}>
+            Test Your Speaking
+          </Link>
+        </li>
+        <li>
+          <Link to="/lessons-reading" onClick={() => setMenuOpen(false)}>
+            Test Your Reading
+          </Link>
+        </li>
+        <li>
+          <Link to="/important-figures" onClick={() => setMenuOpen(false)}>
+            Important Figures
+          </Link>
+        </li>
+        <li>
+          <Link to="/important-events" onClick={() => setMenuOpen(false)}>
+            Important Events
+          </Link>
+        </li>
+        <li>
+          <Link to="/values-ethics" onClick={() => setMenuOpen(false)}>
+            Values & Ethics
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
